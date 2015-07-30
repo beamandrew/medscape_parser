5c5b05ca15cc62f2b35321225988da203bdabeaa
beamandrew <beam.andrew@gmail.com>
2015-07-30 15:42:15 -0400

    first parser functionality implemented

--- scripts/create_page_manifest.R
+++ scripts/create_page_manifest.R
@@ -11,3 +11,30 @@ for(i in 1:length(base_links)) {
   sections <- rbind(sections,cbind(name,link))
 }
 
+excluded_sections <- c("Cancer Guidelines","Cancer Treatment & Staging Guides ")
+
+medscape_links <- NULL
+## Now go through and get the links for all of the pages in each section ##
+for(i in 1:nrow(sections)) {
+  print(i)
+  subsection <- sections[i,]
+  section <- as.character(subsection$name)
+  link <- as.character(subsection$link)
+  if( length(which(excluded_sections == section)) == 0 ) {
+    if( length(grep("http",link)) == 0 ) {
+      section_page <- html(paste0(baseurl,link))
+    } else {
+      section_page <- html(link)
+    }  
+    section_links <- section_page %>% html_node("div.midcol") 
+    if(is.null(section_links)) {
+      section_links <- section_page %>% html_node("div.maincolbox") 
+    }
+    section_links <- section_links %>% html_nodes("li") 
+    for(j in 1:length(section_links)) {
+      name <- section_links[[j]] %>% html_text()
+      link <-  section_links[[j]] %>% html_node("a") %>% html_attr("href")
+      medscape_links <- rbind(medscape_links,cbind(section,name,link))
+    }
+  }
+}
